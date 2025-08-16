a3fddd8f650175987e126e67e730908aac1208d3
Patrick Marcus <patrickmarcus1144@gmail.com>
2025-08-16 23:42:25 +0000

    the whatsapp icon logo inside is too small i barely recognize if it what

--- src/components/common/whatsapp-button.tsx
+++ src/components/common/whatsapp-button.tsx
@@ -9,8 +9,8 @@ interface WhatsAppButtonProps {
 const WhatsAppIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="32"
-    height="32"
+    width="40"
+    height="40"
     viewBox="0 0 24 24"
     fill="currentColor"
   >
